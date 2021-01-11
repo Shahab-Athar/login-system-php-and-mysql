f95ccbbc72f500ad503c1da21d8f3d7f69252205
Shahab Athar <shahabathar103@gmail.com>
2021-01-11 22:30:42 +0500

    Add Basic Style And Form To Login System

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Login System</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+        <h2>Login</h2>
+        <label>Username: </label>
+        <input type="text" name="unname" placeholder="Username">
+        <br>
+
+        <label>Password: </label>
+        <input type="passowrd" name="password" placeholder="Password">
+        <br>
+        
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
